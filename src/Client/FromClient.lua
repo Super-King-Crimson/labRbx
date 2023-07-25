b7681552d6ed1f725606e583166f93c8a9642ac4
Super-King-Crimson <129329889+Super-King-Crimson@users.noreply.github.com>
2023-07-24 22:51:20 -0400

    roblox.

--- src/Client/FromClient.lua
+++ src/Client/FromClient.lua
@@ -1,1 +1,11 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Packages = ReplicatedStorage.Packages
+local SleitSignal = require(Packages.SleitSignal)
+
 print("Hi I'm Client")
+
+local signal = SleitSignal.new()
+
+local conn = signal:Connect()
+
+signal:Fire()
