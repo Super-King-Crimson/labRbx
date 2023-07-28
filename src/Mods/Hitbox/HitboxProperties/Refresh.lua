2ead72081383dc910a6940afd1814a0857410692
Super-King-Crimson <129329889+Super-King-Crimson@users.noreply.github.com>
2023-07-28 07:41:07 -0400

    Created Refresh hitbox component

---
+++ src/Mods/Hitbox/HitboxProperties/Refresh.lua
@@ -0,0 +1,19 @@
+local Refresh = {}
+
+function Refresh.new(onDuration: number, offDuration: number, startsOn: boolean?): RefreshType
+    if startsOn == nil then startsOn = true end
+
+    return {
+        onDuration = onDuration,
+        offDuration = offDuration,
+        startsOn = if startsOn ~= nil then startsOn else false,
+    }
+end
+
+export type RefreshType = {
+    startsOn: boolean,
+    onDuration: number,
+    offDuration: number,
+}
+
+return Refresh
